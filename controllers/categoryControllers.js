22fc0a9469b893c7f6b6e48c5e7f05456a28d2c3
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2024-09-02 10:48:31 +0530

    added all category routes and controllers

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -31,4 +31,90 @@ export const createCategory = async (req, res, next) => {
         console.log(error);
         return next(new ApiError(501, "Failed to create category"))
     }
+};
+
+export const getCategories = async (req, res, next) => {
+    try {
+        const categories = await categoryModel.find();
+        if (!categories) {
+            return next(new ApiError(401, "Categories not fetched"))
+        }
+        res.status(201).json(
+            new ApiResponse(200, categories, "Categories fetched successfully")
+        )
+    } catch (error) {
+        console.log(error);
+        return next(new ApiError(501, "Failed to fetched categories"))
+    }
+};
+
+export const getCategory = async (req, res, next) => {
+    const { cid } = req.params;
+    if (!cid) {
+        return next(new ApiError(401, "Category Id is required"))
+    }
+    try {
+        const category = await categoryModel.findById(cid);
+        if (!category) {
+            return next(new ApiError(402, "Category not found"))
+        }
+        res.status(201).json(
+            new ApiResponse(200, category, "Category fetched successfully")
+        )
+    } catch (error) {
+        console.log(error);
+        return next(new ApiError(501, "Failed to fetched category"))
+    }
+}
+
+export const updateCategory = async (req, res, next) => {
+    const { cid } = req.params;
+    if (!cid) {
+        return next(new ApiError(401, "Category Id is required"))
+    }
+    const { name } = req.body;
+    try {
+        const category = await categoryModel.findById(cid);
+        if (!category) {
+            return next(new ApiError(401, "Category Id is not valid"))
+        }
+
+        const updatedCategory = await categoryModel.findByIdAndUpdate(cid, {
+            name: name || category.name,
+            image: {}
+        }, { new: true });
+
+        if (req.file) {
+            const imageLocalPath = req.file.path;
+            const image = await uploadCloudinary(imageLocalPath);
+            if (!image) {
+                next(new ApiError(400, "Something went wrong"));
+            }
+            updatedCategory.image.public_id = image.public_id || category.image.public_id;
+            updatedCategory.image.secure_url = image.secure_url || category.image.secure_url;
+        }
+        await updatedCategory.save();
+        res.status(201).json(
+            new ApiResponse(200, updatedCategory, "Category updated successfully")
+        )
+    } catch (error) {
+        console.log(error);
+        return next(new ApiError(501, "Failed to update category"))
+    }
+}
+
+export const deleteCategory = async (req, res, next) => {
+    const { cid } = req.params;
+    if (!cid) {
+        return next(new ApiError(401, "Category Id is required"))
+    }
+    try {
+        await categoryModel.findByIdAndDelete(cid);
+        res.status(201).json(
+            new ApiResponse(200, "Category deleted successfully")
+        )
+    } catch (error) {
+        console.log(error);
+        return next(new ApiError(501, "Failed to deleted category"))
+    }
 }
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import upload from "../middlewares/multerMiddleware.js";
-import { createCategory } from "../controllers/categoryControllers.js";
+import { createCategory, deleteCategory, getCategories, getCategory, updateCategory } from "../controllers/categoryControllers.js";
 
 const categoryRoutes = Router();
 
 categoryRoutes.post("/", upload.single("image"), createCategory);
+categoryRoutes.get("/", getCategories);
+categoryRoutes.get("/:cid", getCategory);
+categoryRoutes.put("/:cid", upload.single("image"), updateCategory);
+categoryRoutes.delete("/:cid", deleteCategory);
 
 export default categoryRoutes;
